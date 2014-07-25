4d6a590a0f60e6e1990eadb745dc93a96309235b
Marcell Jusztin <marcell.jusztin@flashtalking.com>
2014-07-25 17:01:15 +0100

    Socket connection unit test

--- socketconn_test.go
+++ socketconn_test.go
@@ -3,3 +3,10 @@ package main
 import (
 	"testing"
 )
+
+func TestStartSocketConnection_invalidPath(t *testing.T) {
+	sc := new(SocketConnection)
+	if err := sc.StartSocketConnection("/invalid/path.sock"); err == nil {
+		t.Fatalf("should have thrown an error")
+	}
+}
